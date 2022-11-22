d2577e8a4550d822ae4eda5229ab544742eb1eeb
KinksterFox <103235272+KinksterFox@users.noreply.github.com>
2022-11-22 10:21:13 +0100

    modified:   carousel/images.json new file:   carousel/images/joltycon_gekkouai.png modified:   carousel/src/css/animations.css modified:   carousel/src/script.js modified:   carousel/src/style.css modified:   carousel/userconfig.json

--- carousel/src/script.js
+++ carousel/src/script.js
@@ -93,9 +93,7 @@ function populateCarousel(obj) {
             carousal.appendChild(slide);
         }
     }
-    window.onload = () => {
         showSlides();
-    }
 }
 
 // Carousel Functionality
