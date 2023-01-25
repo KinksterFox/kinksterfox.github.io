c4b2cc3b01d8b1d31b4ec4bd92d9da00ed7c16ef
KinksterFox <joltmc@outlook.com>
2023-01-25 11:02:56 +0100

    modified:   api/custom-tags/custom-tags.js
    	modified:   api/custom-tags/kinksterfox-tags.json

--- api/custom-tags/custom-tags.js
+++ api/custom-tags/custom-tags.js
@@ -1,10 +1,14 @@
+// ----------------------------
+//    Custom Tag API Variables
+// ----------------------------
 const TAG_API = {
   url: (username) => `https://github.kinksterfox.com/api/custom-tags/${username}-tags.json`,
   tag: "tag",
 };
 
-
-
+// ----------------------------
+//    Custom Tag API Functions
+// ----------------------------
 async function getCustomTag(username) {
   const res = await fetch(`${TAG_API.url}`).then((response) => {
     return response.json();
