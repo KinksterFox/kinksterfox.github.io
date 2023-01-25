ab60379244b7cd2afe6b7a1f691f7725254eadf8
KinksterFox <joltmc@outlook.com>
2023-01-25 13:17:36 +0100

    modified:   api/custom-tags/custom-tags.js

--- api/custom-tags/custom-tags.js
+++ api/custom-tags/custom-tags.js
@@ -2,18 +2,32 @@
 //    Custom Tag API Variables
 // ----------------------------
 const TAG_API = {
-  url: (username) => `https://github.kinksterfox.com/api/custom-tags/${username}-tags.json`,
-  tag: "tag",
+  base: "https://github.kinksterfox.com/api/custom-tags/",
+  channel: "",
+  tag: "",
+  tagCache: ""
 };
 
 // ----------------------------
 //    Custom Tag API Functions
 // ----------------------------
-async function getCustomTag(username) {
-  const res = await fetch(`${TAG_API.url}`).then((response) => {
+async function getCustomTag(channelname, messenger) {
+  TAG_API.tag = "";
+  TAG_API.tagCache = "";
+  
+  TAG_API.channel = `${channelname}-tags.json`;
+  const res = await fetch(`${TAG_API.base}${TAG_API.channel}`).then((response) => {
     return response.json();
   });
-
-  TAG_API.tag = res.find(({ channelname }) => channelname === username).custom_tag;
+  
+  TAG_API.tagCache = res.find(({ nick }) => nick === messenger);
+  
+  if (TAG_API.tagCache != undefined) {
+    TAG_API.tag = TAG_API.tagCache.custom_tag;
+  }
+    
+  if (!TAG_API.tag) {
+    return null;
+  }
   return TAG_API.tag
 }
